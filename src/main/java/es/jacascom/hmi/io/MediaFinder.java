43556992e058a7cee9004beeea5664de32fb3c4c
Javier Castro <elkstro@gmail.com>
2012-02-28 22:47:22 +0100

    Created List of MediaFile from a path

--- src/main/java/es/jacascom/hmi/io/MediaFinder.java
+++ src/main/java/es/jacascom/hmi/io/MediaFinder.java
@@ -1,14 +1,41 @@
 package es.jacascom.hmi.io;
 
-import java.util.List;
 import es.jacascom.hmi.data.MediaFile;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MediaFinder implements IMediaFinder {
 
+
     public List<MediaFile> findMedia(String path) {
+
+        File mFolder = new File(path);
+
+        File [] mFiles = mFolder.listFiles();
+
+        List<MediaFile> list = new ArrayList<MediaFile>();
+
+        if (mFiles == null) {
+
+        }   else {
+            for (int i=0; i< mFiles.length; i++){
+                // If mFiles is a Directory, then goes in and check mFiles
+                if (mFiles[i].isDirectory()) {
+                    List<MediaFile> dirFiles = findMedia(mFiles[i].getAbsolutePath());
+                    list.addAll(dirFiles);
+                } else {
+                    // Creates a MediaFile with mFiles[i]
+                    MediaFile f = new MediaFile("CRC",mFiles[i].getName(),mFiles[i].getPath(),"Title",mFiles[i].length());
+                    list.add(f);
+                }
+            }
+        }
         // TODO
-        return null;
+        return list;
     }
+
 
     public List<MediaFile> findMedia(String path, String[] extensions) {
         // TODO
