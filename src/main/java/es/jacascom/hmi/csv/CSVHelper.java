8ded418dd43298092abff7ece2782fbaf2676908
Paco Castro <paco_castro.tech@yahoo.es>
2012-03-07 11:19:21 +0100

    Base files for CSV usage

---
+++ src/main/java/es/jacascom/hmi/csv/CSVHelper.java
@@ -0,0 +1,21 @@
+package es.jacascom.hmi.csv;
+
+import es.jacascom.hmi.data.MediaFile;
+
+public final class CSVHelper {
+
+    private CSVHelper() {
+    }
+
+    public static MediaFile convertToMediaFile(String[] row) {
+        return null;
+    }
+
+    public static String[] convertFromMediaFile(MediaFile media) {
+        return null;
+    }
+
+    public static String[] extractRowHeaders(Class beanClass) {
+        return null;
+    }
+}
---
+++ src/main/java/es/jacascom/hmi/csv/ICSVManager.java
@@ -0,0 +1,23 @@
+package es.jacascom.hmi.csv;
+
+import java.util.List;
+import java.io.IOException;
+import es.jacascom.hmi.data.MediaFile;
+
+public interface ICSVManager {
+
+    /**
+     * Find MediaFiles in a given folder
+     * 
+     * @param path Root folder
+     */
+    List<MediaFile> loadCSV(String path) throws IOException;
+
+    /**
+     * Find MediaFiles in a given folder
+     * 
+     * @param path Root folder
+     * @param extensions Extensions to be filtered
+     */
+    boolean saveCSV(String path, String[] extensions) throws IOException;
+}
